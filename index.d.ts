5b6d8ea200b768cdb629cc870e5801a84f676bdf
Lukester <fogbpl@gmail.com>
2018-04-13 00:59:29 +0200

    Updated typings file

--- index.d.ts
+++ index.d.ts
@@ -33,29 +33,29 @@ declare namespace Safetify {
 
     type ResolverFunction<T> = (input: any) => Result<T>;
 
-    const Boolean: Resolver<boolean>;
+    function Boolean(): Resolver<boolean>;
     type Boolean = Resolver<boolean>;
 
-    const Number: Resolver<number>;
+    function Number(): Resolver<number>;
     type Number = Resolver<number>;
 
-    const String: Resolver<string>;
+    function String(): Resolver<string>;
     type String = Resolver<string>;
 
     type Any = Resolver<any>;
-    const Any: Resolver<any>;
+    function Any(): Resolver<any>;
 
-    const BooleanResolver: Resolver<boolean>;
+    function BooleanResolver(): Resolver<boolean>;
     type BooleanResolver = Resolver<boolean>;
 
-    const NumberResolver: Resolver<number>;
+    function NumberResolver(): Resolver<number>;
     type NumberResolver = Resolver<number>;
 
-    const StringResolver: Resolver<string>;
+    function StringResolver(): Resolver<string>;
     type StringResolver = Resolver<string>;
 
     type AnyResolver = Resolver<any>;
-    const AnyResolver: Resolver<any>;
+    function AnyResolver(): Resolver<any>;
     
 
     type ObjectResolverDefinition<T> = { [U in keyof T]: Resolver<T[U]> }
