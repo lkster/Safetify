bb90aab4b7e26e2c6302e8d28a5972b2f0f2de5f
Lukester <fogbpl@gmail.com>
2018-04-13 00:58:59 +0200

    Changed all variable-based resolvers to function-based

--- src/resolvers/Boolean.ts
+++ src/resolvers/Boolean.ts
@@ -4,13 +4,14 @@ import { Resolver } from '../Resolver';
 import { Result } from '../Result';
 
 
-
-export const BooleanResolver: Resolver<boolean> = new Resolver<boolean>('boolean', (input: any) => {
-    let error: string;
-
-    if (!Util.isBoolean(input)) {
-        error = 'value is not a boolean';
-    }
-
-    return new Result<boolean>(!Util.isDefAndNotNull(error), !!input, error);
-});
+export function BooleanResolver(): Resolver<boolean> {
+    return new Resolver<boolean>('boolean', (input: any) => {
+        let error: string;
+    
+        if (!Util.isBoolean(input)) {
+            error = 'value is not a boolean';
+        }
+    
+        return new Result<boolean>(!Util.isDefAndNotNull(error), !!input, error);
+    });
+}
