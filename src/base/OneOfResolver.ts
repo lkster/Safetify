6f412c7d09f026fea9509318685f3cb20ee9d9e1
Lukester <fogbpl@gmail.com>
2018-04-14 19:54:59 +0200

    Implemented OneOfResolver class

---
+++ src/base/OneOfResolver.ts
@@ -0,0 +1,13 @@
+import { Resolver } from "./Resolver";
+import { Result } from "@/Result";
+import { Util } from "@/utils/Util";
+import { ResolverFunction } from "@/ResolverFunction";
+
+
+
+export class OneOfResolver<T> extends Resolver<T> {
+
+    constructor (resolver: ResolverFunction<T>) {
+        super('oneof', resolver);
+    }
+}
