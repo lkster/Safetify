d47893e03c6e932d8ddc037454ba2c594c66bcbc
Lukester <fogbpl@gmail.com>
2018-05-10 23:47:48 +0200

    Added new build

--- dist/safetify.js
+++ dist/safetify.js
@@ -70,47 +70,113 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * @hidden
+/*
+ * Based on Closure Library's Goog Functions by Google
+ * https://github.com/google/closure-library
  */
 var Util = /** @class */ (function () {
     function Util() {
     }
+    /**
+     * Returns true if the specified value is null.
+     * @param val Variable to test.
+     */
     Util.isDef = function (val) {
         return val !== void 0;
     };
+    /**
+     * Returns true if the specified value is defined and not null.
+     * @param val Variable to test.
+     */
     Util.isDefAndNotNull = function (val) {
         return val != null;
     };
+    /**
+     * Returns true if the specified value is a string.
+     * @param {?} val Variable to test.
+     */
     Util.isString = function (val) {
         return typeof val == 'string';
     };
+    /**
+     * Returns true if the specified value is a boolean.
+     * @param {?} val Variable to test.
+     */
     Util.isBoolean = function (val) {
         return typeof val == 'boolean';
     };
+    /**
+     * Returns true if the specified value is a number.
+     * @param val Variable to test.
+     */
     Util.isNumber = function (val) {
         return typeof val == 'number';
     };
+    /**
+     * Returns true if the specified value is an array.
+     * @param val Variable to test.
+     */
     Util.isArray = function (val) {
         return this._typeOf(val) == 'array';
     };
+    /**
+     * Returns true if the object looks like an array. To qualify as array like
+     * the value needs to be either a NodeList or an object with a Number length
+     * property. Note that for this function neither strings nor functions are
+     * considered "array-like".
+     *
+     * @param val Variable to test.
+     */
     Util.isArrayLike = function (val) {
         var type = this._typeOf(val);
         return type == 'array' || type == 'object' && typeof val.length == 'number';
     };
+    /**
+     * Returns true if the specified value is an object.  This includes arrays and
+     * functions.
+     * @param val Variable to test.
+     */
     Util.isObject = function (val) {
         var type = typeof val;
         return type == 'object' && val != null || type == 'function';
     };
+    /**
+     * Returns true if the object looks like a Date. To qualify as Date-like the
+     * value needs to be an object and have a getFullYear() function.
+     * @param val Variable to test.
+     */
     Util.isDateLike = function (val) {
         return this.isObject(val) && typeof val.getFullYear == 'function';
     };
-    Util.isStringValidDate = function (val) {
-        return !isNaN(+new Date(val));
+    /**
+     * Returns true if the specified value is a valid date (can be string,
+     * unix timestamp but also custom library like Moment)
+     * @param val Variable to test
+     */
+    Util.isValidDate = function (val) {
+        return this.isDefAndNotNull(val) && !this.isBoolean(val) && !isNaN(+new Date(val));
     };
+    /**
+     * Returns true if the specified value is a function.
+     * @param val Variable to test.
+     */
     Util.isFunction = function (val) {
         return this._typeOf(val) == 'function';
     };
+    /**
+     * Returns true if the specified value is a dictionary typed object. This means that object is created
+     * only to be a storage without any logic
+     * @param val Variable to test
+     */
+    Util.isDict = function (val) {
+        return this.isObject(val) && val.constructor === Object;
+    };
+    /**
+     * @hidden
+     * This is a "fixed" version of the typeof operator.  It differs from the typeof
+     * operator in such a way that null returns 'null' and arrays return 'array'.
+     * @param val The value to get the type of.
+     */
     Util._typeOf = function (val) {
         var s = typeof val;
         if (s == 'object') {
@@ -854,6 +920,8 @@ var Resolver_1 = __webpack_require__(2);
 exports.Resolver = Resolver_1.Resolver;
 var Result_1 = __webpack_require__(1);
 exports.Result = Result_1.Result;
+var Util_1 = __webpack_require__(0);
+exports.util = Util_1.Util;
 
 
 /***/ }),
