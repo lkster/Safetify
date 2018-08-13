30573160c4335341c15787e728f269c4beb65f5f
Lucas Maltster <fogbpl@gmail.com>
2018-08-13 21:44:19 +0200

    Moved optional and nullable methods to separated NullableResolver and OptionalResolver abstract classes

---
+++ src/base/OptionalResolver.ts
@@ -0,0 +1,28 @@
+import { NullableResolver } from '@/base/NullableResolver';
+import { Resolver } from '@/base/Resolver';
+import { Result } from '@/Result';
+import { Util } from '@/utils/Util';
+
+
+
+export abstract class OptionalResolver<T> extends NullableResolver<T> {
+
+    /**
+     * @hidden
+     */
+    protected isOptional: boolean = false;
+
+    public resolve(input: any): Result<T> {
+        if (this.isOptional && (Util.isNull(input) || !Util.isDef(input))) {
+            return new Result<T>(true, null, []);
+        }
+
+        return super.resolve(input);
+    }
+    
+    public optional(): Resolver<T> {
+        this.isOptional = true;
+        this.isNullable = true;
+        return this;
+    }
+}
