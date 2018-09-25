913d2ed97148ad4008f5900e8d87f7e14fd654e8
Lucas Maltster <fogbpl@gmail.com>
2018-09-25 23:59:36 +0200

    Fixed setting to null advanced types even if they are correct in half

--- src/base/TupleResolver.ts
+++ src/base/TupleResolver.ts
@@ -1,6 +1,8 @@
 import { ITuple } from '@/interfaces/ITuple';
 import { Resolver } from '@/base/Resolver';
 import { ResolverFunction } from '@/ResolverFunction';
+import { Result } from '@/Result';
+import { Util } from '@/utils/Util';
 
 
 
@@ -11,5 +13,24 @@ export class TupleResolver<T extends ITuple> extends Resolver<T> {
      */
     constructor (resolver: ResolverFunction<T>) {
         super('tuple', resolver);
+    }
+
+    /**
+     * @hidden
+     */
+    public resolve(input: any): Result<T> {
+        let resolved = this.resolver(input);
+
+        if (!resolved.success) {
+            if (this.isNullable === true && input === null) {
+                return new Result<T>(true, null, null);
+            } else if (this.isNullable === true && !Util.isArrayLike(input)) {
+                resolved.result = null;
+            }
+        } else if (!Util.isDef(resolved.result) && this.isNullable === true) {
+            resolved.result = null;
+        }
+
+        return resolved;
     }
 }
