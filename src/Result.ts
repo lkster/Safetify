eed04f059bb4434b04ee04503b2291c9936ab75e
Dominik Mielcarek <fogbpl@gmail.com>
2019-02-17 04:02:50 +0100

    Added jsdoc about optionalData param

--- src/Result.ts
+++ src/Result.ts
@@ -10,6 +10,7 @@ export class Result<T> {
      * @param success true if data is successfuly resolved, false otherwise
      * @param result resolved data
      * @param error has error(s) if resolving failed
+     * @param optionalData any data you want to stick to this object. Mostly used to pass data between resolver handlers.
      */
     public constructor (
         public success: boolean,
